baa849248a88893be3dcc74ecd9d1b92bcedc327
Sathwik <hsathwik002@gmail.com>
2024-10-01 19:12:56 +0530

    [Main TestCase TDP_header]

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,21 @@
 import * as dgram from "dgram";
+import DNSHeader, { OpCode, ResponseCode, TDNSHeader } from "../dns/header";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-
+const defaultheaders: TDNSHeader = {
+    id: 1234,
+    qr: 1 << 15,
+    opcode: OpCode.STANDARD_QUERY,
+    aa: 0,
+    tc: 0,
+    rd: 0,
+    ra: 0,
+    z: 0,
+    rcode: ResponseCode.NO_ERROR,
+    qdcount: 0,
+    ancount: 0,
+    nscount: 0,
+    arcount: 0
+}
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
@@ -11,9 +23,12 @@ udpSocket.bind(2053, "127.0.0.1");
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const response = Buffer.from("");
+        const header =DNSHeader.write(defaultheaders);
+        const response = Buffer.concat([header]);
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
     }
 });
+
+
