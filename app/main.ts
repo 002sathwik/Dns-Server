7b688835bdf107f8ab3b606228413a111b8d7028
Sathwik <hsathwik002@gmail.com>
2024-10-01 19:54:35 +0530

    [question_Interface/Questionencoding+testing]

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import * as dgram from "dgram";
 import DNSHeader, { OpCode, ResponseCode, TDNSHeader } from "../dns/header";
+import DNSQuestion, { DNSClass, DNSQuestionType } from "../dns/question";
 
 const defaultheaders: TDNSHeader = {
     id: 1234,
@@ -17,14 +18,22 @@ const defaultheaders: TDNSHeader = {
     arcount: 0
 }
 
+const defaultQuestion = {
+    name: "codecrafters.io",
+    classCode: DNSClass.IN,
+    type: DNSQuestionType.A
+
+}
+
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
 
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const header =DNSHeader.write(defaultheaders);
-        const response = Buffer.concat([header]);
+        const header = DNSHeader.write({ ...defaultheaders, qdcount: 1 });
+        const question = DNSQuestion.write([defaultQuestion]);
+        const response = Buffer.concat([header, question]);
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
