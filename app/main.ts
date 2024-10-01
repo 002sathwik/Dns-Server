631ad2d5a93787c8e61413a92df9695f59f212ff
Sathwik <hsathwik002@gmail.com>
2024-10-01 21:17:58 +0530

    feath:Answer_Passing[Dns]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import * as dgram from "dgram";
 import DNSHeader, { OpCode, ResponseCode, TDNSHeader } from "../dns/header";
 import DNSQuestion, { DNSClass, DNSType } from "../dns/question";
+import DNSAnswer from "../dns/answer";
 
 const defaultheaders: TDNSHeader = {
     id: 1234,
@@ -24,6 +25,14 @@ const defaultQuestion = {
     type: DNSType.A
 
 }
+const defaultAnswer = {
+    name: "codecrafters.io",
+    classCode: DNSClass.IN,
+    type: DNSType.A,
+    ttl: 60,
+    data: "\x7F\x00\x00\x01"
+
+}
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
@@ -31,9 +40,10 @@ udpSocket.bind(2053, "127.0.0.1");
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const header = DNSHeader.write({ ...defaultheaders, qdcount: 1 });
+        const header = DNSHeader.write({ ...defaultheaders, qdcount: 1 , ancount: 1});
         const question = DNSQuestion.write([defaultQuestion]);
-        const response = Buffer.concat([header, question]);
+        const answer = DNSAnswer.write([defaultAnswer]);
+        const response = Buffer.concat([header, question , answer]);
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
